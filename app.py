733d200a649211d1c4aea930cc2b16393862d9d8
Ranil <gabnasyrov03@gmail.com>
2022-12-08 16:20:56 +0300

    second com

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import threading
 
 class EditWin(QMainWindow, edit.Ui_MainWindow):
 
-    def __init__(self):
+    def __init__(self,):
         super(EditWin, self).__init__()
         self.setupUi(self)
-        
+        self.username = ''
+        self.pushButton.clicked.connect(lambda x: print(self.username))
 
 class GameWin(QMainWindow, game.Ui_MainWindow):
 
@@ -23,12 +24,23 @@ class StartWin(QMainWindow, start.Ui_MainWindow):
     def __init__(self):
         super(StartWin, self).__init__()
         self.setupUi(self)
+        self.edit_page = EditWin()
+
+        self.pushButton.clicked.connect(self.go_to_edit_page)
+
+    def go_to_edit_page(self):
+        username = self.lineEdit.text()
+        if not username:
+            return
+        self.edit_page.username = username
+        self.edit_page.show()
+        self.close()
 
 
 if __name__ == '__main__':
 
     app = QApplication([])
-    window = EditWin()
+    window = StartWin()
     window.show()
     app.exec()
 
