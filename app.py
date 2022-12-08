60f53d1cc0ebbbb08d8d879338b1943f96712ca4
Ranil <gabnasyrov03@gmail.com>
2022-12-09 01:15:08 +0300

    Added checkbox and grid headers

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from win_py import edit, game, start
 from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6 import QtWidgets
 import socket
 import threading
 
@@ -11,12 +12,22 @@ class EditWin(QMainWindow, edit.Ui_MainWindow):
         self.setupUi(self)
         self.username = ''
         self.pushButton.clicked.connect(lambda x: print(self.username))
+        self.edit_scene = QtWidgets.QGraphicsScene()
+        self.graphicsView.setScene(self.edit_scene)
+
+    def grid(self):
+        pass
+
 
 class GameWin(QMainWindow, game.Ui_MainWindow):
 
     def __init__(self):
         super(GameWin, self).__init__()
         self.setupUi(self)
+        self.scene_main = QtWidgets.QGraphicsScene()
+        self.scene_en = QtWidgets.QGraphicsScene()
+        self.graphicsView.setScene(self.scene_main)
+        self.graphicsView_2.setScene(self.scene_en)
 
 
 class StartWin(QMainWindow, start.Ui_MainWindow):
